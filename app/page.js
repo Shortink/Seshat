fdda1100eb25b926c08da2d7fdcf1db1e69a43df
short <21148306+Shortink@users.noreply.github.com>
2024-08-01 02:08:20 -0600

    install

--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
+import Link from 'next/link';
 
 
-
-export default function Page() {
+export default function Home() {
+  const searchStyle = ""
+  const searchButton = ""
   return(
-    <main>
+    <main className="h-screen bg-gray-400">
       <h1>Page</h1>
+      {/* <label>Search Books</label> */}
+
+      <div className="">
+        <div>Search Bar</div>
+        <div>Search Result</div>
+        <input type='search'  placeholder="Search Books" className={searchStyle}/>
+        <button type="submit" className={searchButton} >Search</button>
+      </div>
     </main>
   );
 }
